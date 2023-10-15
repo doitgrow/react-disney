5730fce277144a6f50c3d908672d4795f6d74077
Susung Kim <doitgrow@gmail.com>
2023-10-15 22:40:44 +0900

    Swiper 모듈 추가

--- src/api/request.js
+++ src/api/request.js
@@ -2,11 +2,11 @@ const requests = {
     fetchNowPlaying: "/movie/now_playing",
     fetchTrending: "/trending/all/week",
     fetchTopRated: "/movie/top_rated",
-    fetchActionMovies: "/discover/movie?with_genre=28",
-    fetchComedyMovies: "/discover/movie?with_genre=35",
-    fetchHorrorMovies: "/discover/movie?with_genre=27",
-    fetchRomanceMovies: "/discover/movie?with_genre=10749",
-    fetchDocumentaries: "/discover/movie?width_genre=99",
+    fetchActionMovies: "/discover/movie?with_genres=28",
+    fetchComedyMovies: "/discover/movie?with_genres=35",
+    fetchHorrorMovies: "/discover/movie?with_genres=27",
+    fetchRomanceMovies: "/discover/movie?with_genres=10749",
+    fetchDocumentaries: "/discover/movie?width_genres=99",
 };
 
 export default requests;
